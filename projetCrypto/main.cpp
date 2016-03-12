8f9921f7f42a250b1f8efe6046b271fd41bc2c0a
LHF <louis-henri.franc@insa-rouen.fr>
2016-03-12 10:51:17 +0100

    euclide étendu

--- projetCrypto/main.cpp
+++ projetCrypto/main.cpp
@@ -1,45 +1,24 @@
 #include <iostream>
 #include <cstdlib>
+#include <cassert>
 #include "BigInteger.h"
 #include "RSAInterface.h"
 #include "BigIntegerUtils.h"
-#include "MillerRabin.h"
-
-static BigInteger pi(BigInteger n)
-{
-  BigInteger r=0, m=2;
-
-  while ( m < n ){
-    if ( isprime(m) ) ++r;
-    m++;
- }
-  return r;
-}
+#include "MathCrypto.h"
 
 
 int main()
 {
- srand(time(NULL));
- int expected[] = {0, 4, 25, 168, 1229, 9592, 78498, 664579};
-std::cout << (isprime(8000309) == true ? "oui" : "non");
-//for ( BigInteger n=1, e=0; n<=10000000; n*=10, ++e ) {
-//   BigInteger primes = pi(n);
-//   std::cout << "there are " << primes << "less than " << n <<std::endl;
-//
-//   if ( primes.toInt() == expected[e.toInt()] ) printf("--ACHIEVE");
-//   else printf(" --- FAIL");
-// }
- //  srand(time(NULL));
- //  std::string s("1000000000000000000000000000000000000");
-  //  BigInteger a = stringToBigInteger(s);
-  //  s="20000000000000000000000000000000000089954354";
-  //  BigInteger b = stringToBigInteger(s);
-  //  RSAInterface RSA;
-//
-//  //  bool isPrime=RSA.Miller(21,5);
-//  //  std::cout << ((isPrime == true) ? "oui" : "non");
-  //  std::cout << " " << bgg(6,5,21);
-    // std::cout << d;
+  srand(time(NULL));
+  BigInteger x(4918615616);
+  BigInteger y(4918615616);
+  x.pow(3);
+  y.pow(4);
+  BigInteger a,b;
+  std::cout << math_crypto::Extended_Euclid_GCD(x,y,a,b) << " " << a << " " << b << std::endl;
+  std::cout << math_crypto::Euclid_GCD(x,y)<<std::endl;
+  std::cout << x*a+y*b;
+  //assert(x*a + y*b = math_crypto::Euclid_GCD(x,y));
 }
 
 
