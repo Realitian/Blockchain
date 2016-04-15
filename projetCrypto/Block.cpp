7678513c5e153e8d83c2c2d6af868d23019071b7
lh <louis.franc@insa-rouen.fr>
2016-04-15 11:41:21 -0400

    implementation classe blockchain

--- projetCrypto/Block.cpp
+++ projetCrypto/Block.cpp
@@ -1,6 +1,18 @@
 #include "Block.h"
 
-
+// bool setLastBlock(ptr_Block);
+// bool isValid() const;
+// bool containsTransactions(const Transaction&) const;
+// void setSize();
+// void BuildMerkleRoot();
+// void solveProofofWork();
+// ptr_Block getParent();
+// private:
+// 	ptr_Block header; // c'est un std::shared_ptr<>
+// 	ptr_Block previousBlock; // mis dans block pour éviter inclusion circulaire
+// 	__int8 nombreTransaction;
+// 	__int8 tailleBlock;
+// 	vector<Transaction> transactions; // Ce n'est pas les transactions, mais les hashs
 
 Block::Block()
 {
@@ -10,3 +22,4 @@ Block::Block()
 Block::~Block()
 {
 }
+
--- projetCrypto/BlockChain.cpp
+++ projetCrypto/BlockChain.cpp
@@ -11,3 +11,22 @@ BlockChain::BlockChain()
 BlockChain::~BlockChain()
 {
 }
+
+
+bool BlockChain::addBlock(const Block& bloc)
+{
+	if (std::find(blocks.begin(), blocks.end(),bloc) != blocks.end())
+		return false;
+	blocks.push_back(bloc);
+	return true;
+}
+
+bool BlockChain::checkTransactionExist(const Transaction& trans)
+{
+	if (std::any_of(blocks.rbegin(), blocks.rend(), [&trans](const Block& bloc) {
+		return bloc.containsTransactions(trans);
+	}))
+		return true;
+	else
+		return false;
+}
